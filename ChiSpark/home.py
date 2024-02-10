cd11bc12b896876b256012e77ba6b13433f8525d
Grigory Kozhanov <kogriv@gmail.com>
2024-02-10 19:48:26 +0700

    path find

---
+++ ChiSpark/home.py
@@ -0,0 +1,13 @@
+import os
+print(os.environ['HOME'])
+#print(os.environ['USERPROFILE'])
+
+count_directories = 0
+count_files = 0
+
+for root, dirs, files in os.walk("/work"):
+    count_directories += len(dirs)
+    count_files += len(files)
+
+print("Количество обработанных каталогов:", count_directories)
+print("Количество обработанных файлов:", count_files)
--- ChiSpark/norm_path.py
+++ ChiSpark/norm_path.py
@@ -1,11 +1,10 @@
 import subprocess
 import os
+import sys
 
 import ctypes
 from ctypes import wintypes
 
-# Загрузка библиотеки kernel32.dll
-kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
 
 # Определение структуры WIN32_FIND_DATA
 class WIN32_FIND_DATA(ctypes.Structure):
@@ -23,39 +22,56 @@ class WIN32_FIND_DATA(ctypes.Structure):
     ]
 
 def get_actual_folder_name(path):
-    # Инициализация структуры WIN32_FIND_DATA
-    find_data = WIN32_FIND_DATA()
-    # Получение дескриптора папки
-    hFind = kernel32.FindFirstFileW(path, ctypes.byref(find_data))
-    # Проверка на успешное открытие папки
-    if hFind != -1:
-        # Закрытие дескриптора
-        kernel32.FindClose(hFind)
-        # Возвращение фактического имени папки
-        return find_data.cFileName
+    """
+    Данная функция используется только на виндоус.
+    Принимает путь.
+    Возвращает фактическое имя конечной папки пути с учетом регистра
+    ("cFileName", wintypes.WCHAR * 260)
+    """
+    if sys.platform == 'win32':
+        # Инициализация структуры WIN32_FIND_DATA
+        find_data = WIN32_FIND_DATA()
+        # Загрузка библиотеки kernel32.dll
+        kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+        # Получение дескриптора папки
+        hFind = kernel32.FindFirstFileW(path, ctypes.byref(find_data))
+        # Проверка на успешное открытие папки
+        if hFind != -1:
+            # Закрытие дескриптора
+            kernel32.FindClose(hFind)
+            # Возвращение фактического имени папки
+            return find_data.cFileName
+    else:
+        return path
 
 def get_actual_path(folder_path):
-    # Удаление слэша в конце пути, если есть
-    if folder_path.endswith('\\'):
-        folder_path = folder_path[:-1]
+    """
+    Данная функция используется только на виндоус.
+    """
+    if sys.platform == 'win32':
+        # Удаление слэша в конце пути, если есть
+        if folder_path.endswith('\\'):
+            folder_path = folder_path[:-1]
 
-    #print("folder_path:",folder_path)
-    # Разбиваем путь на компоненты
-    path_components = folder_path.split('\\')
-    actual_path = ''
-    for component in path_components:
-        #print("component:",component)
-        if component:
-            # Если компонент содержит только одну букву и двоеточие,
-            # добавляем его к актуальному пути без вызова get_actual_folder_name
-            if len(component) == 2 and component[1] == ':':
-                actual_path += component
-            else:
-                actual_path = '\\'.join([actual_path, get_actual_folder_name(actual_path + '\\' + component)])
-                if actual_path is None:
-                    return None
-        #print(actual_path)
-    return actual_path
+        #print("folder_path:",folder_path)
+        # Разбиваем путь на компоненты
+        path_components = folder_path.split('\\')
+        actual_path = ''
+        for component in path_components:
+            #print("component:",component)
+            if component:
+                # Если компонент содержит только одну букву и двоеточие,
+                # добавляем его к актуальному пути без вызова get_actual_folder_name
+                if len(component) == 2 and component[1] == ':':
+                    actual_path += component
+                else:
+                    actual_path = '\\'.join([actual_path, get_actual_folder_name(actual_path + '\\' + component)])
+                    if actual_path is None:
+                        return None
+            #print(actual_path)
+        return actual_path
+    else:
+        return folder_path
 
 def universalize_path(input_path, standard_path=None):
     if standard_path is None:
